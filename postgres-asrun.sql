b4f3549a2628f2658eedc03d0c955c2138e8141a
RABC <RABCbot@users.noreply.github.com>
2020-04-28 14:09:56 -0400

    Update postgres-asrun.sql

--- postgres-asrun.sql
+++ postgres-asrun.sql
@@ -1,5 +1,15 @@
-### query an asrun channel by guid by date
-select data->'Start'->'Timecode', data->'PrimaryContent'->'HouseId', data->'SegmentNumber', 
-data->'Title', data->'Duration', data->'AsRunStatus', data->'ReconcileKey' 
+### query an asrun channel guid by date
+
+select data->'Start'->'Date' as StartDate,
+data->'Start'->'Timecode' as StartTime,
+data->'PrimaryContent'->'HouseId' as houseId,
+data->'PrimaryContent'->'SegmentNumber' as Seg,
+data->'PrimaryContent'->'Title' as Title,
+data->'PrimaryContent'->'Filename' as Filename,
+data->'PrimaryContent'->'SOM' as SOM,
+data->'PrimaryContent'->'Duration' as Duration,
+data->'AsRunStatus' as Status,
+data->'StartMode' as StartMode,
+data->'ReconcileKey' as Reconcile
 from public."<asrun-guid>" 
 where data->'Start' @> '{"Date":"2020-01-01T00:00:00"}'
