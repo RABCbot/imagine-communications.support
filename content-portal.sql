3c4871727a6d4f7c7052abcfe1954b2b590f0dc9
RABC <RABCbot@users.noreply.github.com>
2020-04-28 08:45:58 -0400

    Update content-portal.sql

--- content-portal.sql
+++ content-portal.sql
@@ -3,3 +3,7 @@ select data from public.content where data->>'houseId' = 'your-id'
 
 -- show number of records per registration status
 select data->'registrationStatus', count(data) from public.content group by data->'registrationStatus'
+
+-- count records in a specific endpoint
+select count(data) from public.content where data->>'endpoints' like '%062ae5af-63c9-4052-b469-84f1f6d94c94%'
+
