3671967e96d8cd5553dc403fe5a539c4d38f6c43
Ricardo Balladares <RABCbot@users.noreply.github.com>
2020-04-30 08:12:56 -0400

    Update adc.sql

--- adc.sql
+++ adc.sql
@@ -1,5 +1,7 @@
 -- select one id from the main table
 select * from ASDB.dbo.ASDB where Identifier = 'your-id'
+-- same as above, but search if trailing spaces
+select * from ASDB.dbo.ASDB where Identifier like 'your-id%'
 -- select one id from the segments table 
 select * from ASDB.dbo.ASSEG where Identifier = 'your-id'
 -- select one id from the more info table 
@@ -8,13 +10,12 @@ select * from ASDB.dbo.ASEXT where Identifier = 'your-id'
 select Type, Identifier, Cast(Duration as varbinary(4)), Cast(StartOfMessage as varbinary(4)) from ASDB.dbo.ASDB where Identifier = 'your-id'
 -- show DUR/SOM properly from segmentsn table
 select Type, SegType, SegNumber, Identifier, Cast(Duration as varbinary(4)), Cast(StartOfMessage as varbinary(4)) from ASDB.dbo.ASDB where Identifier = 'your-id'
+-- insert a record with SOM 01:02:03:04 and DUR 00:00:30:00
+insert [ASDB].[dbo].[ASDB] (Type, Identifier, StartOfMessage, Duration)
+values ('s', 'your-id', 0x01020304, 0x00003000); 
 
-DELETE FROM [ASDB].[dbo].[ASDB] where Identifier = 'T018005 ' 
+-- Danger zone, delete a specific record
+DELETE FROM [ASDB].[dbo].[ASDB] where Identifier = 'your-id' 
 GO
-DELETE FROM [ASDB].[dbo].[ASEXT] where Identifier = 'T018005 '
+DELETE FROM [ASDB].[dbo].[ASEXT] where Identifier = 'your-id'
 
-INSERT INTO [ASDB].[dbo].[ASEXT] (Identifier, PlayStart, PlayDuration, ClipStart, ClipDuration, "Type")
-VALUES ('DE_TEST003','00:00:00:00','00:00:40:00','00:00:00:00','00:00:40:00', 's'); 
-
-INSERT INTO [ASDB].[dbo].[ASDB] (Identifier, StartOfMessage, Duration, "Type")
-VALUES ('DE_TEST003','0','1200', 's'); 
