972a73bb18e7ca773eb7cc05c7fb144c4cf25120
Ricardo Balladares <RABCbot@users.noreply.github.com>
2020-08-13 11:59:34 -0400

    Update adc.sql

--- adc.sql
+++ adc.sql
@@ -1,15 +1,41 @@
 -- select one id from the main table
 select * from ASDB.dbo.ASDB where Identifier = 'your-id'
+
 -- same as above, but search if trailing spaces
 select * from ASDB.dbo.ASDB where Identifier like 'your-id%'
+
 -- select one id from the segments table 
 select * from ASDB.dbo.ASSEG where Identifier = 'your-id'
+
 -- select one id from the more info table 
 select * from ASDB.dbo.ASEXT where Identifier = 'your-id'
+
 -- show DUR/SOM properly from main table
-select Type, Identifier, Cast(Duration as varbinary(4)), Cast(StartOfMessage as varbinary(4)) from ASDB.dbo.ASDB where Identifier = 'your-id'
+select Identifier, 
+Title, 
+Format((StartOfMessage & 0xFF000000)/0xFF0000, 'X2') + ':' +
+Format((StartOfMessage & 0x00FF0000)/0x00FF00, 'X2') + ':' +
+Format((StartOfMessage & 0x0000FF00)/0x0000FF, 'X2') + ';' +
+Format((StartOfMessage & 0x000000FF)/0x000001, 'X2'),
+Format((Duration & 0xFF000000)/0xFF0000, 'X2') + ':' +
+Format((Duration & 0x00FF0000)/0x00FF00, 'X2') + ':' +
+Format((Duration & 0x0000FF00)/0x0000FF, 'X2') + ';' +
+Format((Duration & 0x000000FF)/0x000001, 'X2'),
+MadeDateTime
+from ASDB..ASDB
+
 -- show DUR/SOM properly from segmentsn table
-select Type, SegType, SegNumber, Identifier, Cast(Duration as varbinary(4)), Cast(StartOfMessage as varbinary(4)) from ASDB.dbo.ASDB where Identifier = 'your-id'
+select Type, SegType, SegNumber, Identifier, 
+Format((StartOfMessage & 0xFF000000)/0xFF0000, 'X2') + ':' +
+Format((StartOfMessage & 0x00FF0000)/0x00FF00, 'X2') + ':' +
+Format((StartOfMessage & 0x0000FF00)/0x0000FF, 'X2') + ';' +
+Format((StartOfMessage & 0x000000FF)/0x000001, 'X2'),
+Format((Duration & 0xFF000000)/0xFF0000, 'X2') + ':' +
+Format((Duration & 0x00FF0000)/0x00FF00, 'X2') + ':' +
+Format((Duration & 0x0000FF00)/0x0000FF, 'X2') + ';' +
+Format((Duration & 0x000000FF)/0x000001, 'X2'),
+ASDB.dbo.ASSEG
+
 -- insert a record with SOM 01:02:03:04 and DUR 00:00:30:00
 insert [ASDB].[dbo].[ASDB] (Type, Identifier, StartOfMessage, Duration)
 values ('s', 'your-id', 0x01020304, 0x00003000); 
